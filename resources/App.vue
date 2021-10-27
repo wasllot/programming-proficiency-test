788bb419527b1b4b81b2dcdc7fb6f9dd690a1a1b
Asur Bernardo <asur@asurbernardo.com>
2021-10-27 15:25:37 +0200

    Final update for frontend exercise

--- resources/App.vue
+++ resources/App.vue
@@ -1,20 +1,11 @@
 <template>
   <div id="app">
-    <table>
-      <tr>
-        <th>id</th>
-        <th>name</th>
-      </tr>
-      <tr v-for="item in properties" :key="item.name">
-        <td v-text="item.id"/>
-        <td v-text="item.name"/>
-      </tr>
-    </table>
+    WORK HERE
   </div>
 </template>
 
 <script>
-import { users, propertyTypes, properties } from '@/libs/api';
+import { users, propertyTypes, properties } from '@/mocks/api';
 
 export default {
   name: 'App',
