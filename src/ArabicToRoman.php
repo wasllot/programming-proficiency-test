5828d20110142a277a5a86216c8dbda194e1175d
Asur Bernardo <asur@asurbernardo.com>
2021-10-27 13:28:23 +0200

    Add comments to each exercise

--- src/ArabicToRoman.php
+++ src/ArabicToRoman.php
@@ -4,6 +4,13 @@ namespace App;
 
 class ArabicToRoman
 {
+    /**
+     * Receive an arabic number and return a string with its roman counterpart
+     *
+     * @param int $arabicNumber Arabic number to be transformed (e.g. 121)
+     *
+     * @return string The roman number equivalent (e.g. CXXI)
+     */
     public static function transform(int $arabicNumber): string
     {
         $romanNumber = '';
--- src/Bakery.php
+++ src/Bakery.php
@@ -4,6 +4,14 @@ namespace App;
 
 class Bakery
 {
+    /**
+     * Calculate the output of cakes for a giver recipe
+     *
+     * @param array $recipe      Contains the necessary ingredients to make one cake
+     * @param array $ingredients Contains the amount of ingredients you have available to bake
+     *
+     * @return int The number of cakes you can bake
+     */
     public static function calculateOutput(array $recipe, array $ingredients): int
     {
         $numberOfCakes = 0;
