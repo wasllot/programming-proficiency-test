42d70550e9993f6e610b916705e7c3543a815a78
David <davidglezmartin@pm.me>
2021-10-27 11:54:18 +0100

    Moved resources content to root, and some fixes in docker options

--- Dockerfile
+++ Dockerfile
@@ -11,3 +11,5 @@ RUN apk add --no-cache \
 RUN curl -sS https://getcomposer.org/installer -o composer-setup.php
 RUN php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 RUN rm -rf composer-setup.php
+
+EXPOSE 8080
---
+++ resources/libs/api.js
@@ -0,0 +1,84 @@
+export const users = [
+  {
+    id: 1,
+    name: 'user test 1',
+  },
+  {
+    id: 2,
+    name: 'user test 2',
+  },
+];
+
+export const propertyTypes = [
+  {
+    id: 1,
+    name: 'home',
+  },
+  {
+    id: 2,
+    name: 'garage',
+  },
+  {
+    id: 3,
+    name: 'office',
+  },
+];
+
+export const properties = [
+  {
+    id: 1,
+    userId: 1,
+    typeId: 1,
+    name: 'casa 1',
+    rentedFrom: new Date(2020, 1, 22),
+    rentedTo: new Date(2020, 7, 7),
+  },
+  {
+    id: 2,
+    userId: 2,
+    typeId: 1,
+    name: 'casa 2',
+    rentedFrom: null,
+    rentedTo: null,
+  },
+  {
+    id: 3,
+    userId: 1,
+    typeId: 2,
+    name: 'garage 1',
+    rentedFrom: new Date(2020, 3, 9),
+    rentedTo: new Date(2020, 4, 7),
+  },
+  {
+    id: 7,
+    userId: 2,
+    typeId: 2,
+    name: 'garage 2',
+    rentedFrom: new Date(2021, 4, 21),
+    rentedTo: null,
+  },
+  {
+    id: 4,
+    userId: 1,
+    typeId: 3,
+    name: 'office 1',
+    rentedFrom: new Date(2020, 5, 22),
+    rentedTo: null,
+  },
+  {
+    id: 5,
+    userId: 1,
+    typeId: 3,
+    name: 'office 2',
+    rentedFrom: null,
+    rentedTo: null,
+  },
+  {
+    id: 6,
+    userId: 2,
+    typeId: 3,
+    name: 'office 3',
+    rentedFrom: new Date(2021, 2, 2),
+    rentedTo: null,
+  },
+];
